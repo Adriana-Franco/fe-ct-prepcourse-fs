5d682260f0047df6cdadb2da214dd17d97cc68a6
Adriana Franco <136268902+Adriana-Franco@users.noreply.github.com>
2023-07-11 19:32:10 -0300

    ejercicios resueltos M03 y M04

---
+++ M03 JavaScript Fundamentos/prueba.js
@@ -0,0 +1,9 @@
+switch (letra){
+   case "a" :return "Es vocal";
+   case "e" :return "Es vocal";
+   case "i" :return "Es vocal";
+   case "o" :return "Es vocal";
+   case "u" :return "Es vocal";
+   default : return "Dato incorrecto";
+}
+}
